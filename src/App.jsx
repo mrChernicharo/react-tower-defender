a8c43421e054a9c1948393d590357f51f5cdae94
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2022-12-28 09:23:25 -0300

    RAF tick loop works

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { useLoop } from "./hooks/useLoop";
+import { useAnimationFrame } from "./hooks/useAnimationFrame";
 
 function App() {
-  const { isPlaying, toggleSpeed, pause, play, clock, speed } = useLoop(deltaTime => {
-    console.log(deltaTime)
-  });
+  const [playing, setPlaying] = useState(false);
+  const [clock, setClock] = useState(0);
+  const [speed, setSpeed] = useState(1);
+
+  function toggleSpeed() {
+    let newSpeed;
+    if (speed === 1) newSpeed = 2;
+    if (speed === 2) newSpeed = 4;
+    if (speed === 4) newSpeed = 1;
+
+    setSpeed(newSpeed);
+
+    if (playing) {
+      setPlaying(false);
+      setTimeout(() => {
+        setPlaying(true);
+      }, 0);
+    }
+  }
+
+  function handleAnimationStep(tick) {
+    setClock(tick / 60);
+  }
+
+  useAnimationFrame(playing, speed, handleAnimationStep);
 
   return (
     <div className="text-white bg-gray-800 min-h-screen text-center">
-      <div>{clock}</div>
+      
+      <div>{clock.toFixed(1)}</div>
+     
+
+      <div>{playing ? "Playing" : "Paused"}</div>
+      <button className="btn" onClick={() => setPlaying(true)}>
+        Play
+      </button>
+      <button className="btn" onClick={() => setPlaying(false)}>
+        Pause
+      </button>
       <div>{speed} X</div>
-      <div>{isPlaying ? 'Playing' : 'Paused'}</div>
-      <button className="btn" onClick={() => play()}>Play</button>
-      <button className="btn" onClick={() => pause()}>Pause</button>
-      <button className="btn" onClick={() => toggleSpeed()}>Toggle Speed</button>
+      <button className="btn" onClick={() => toggleSpeed()}>
+        Toggle Speed
+      </button>
     </div>
   );
 }
---
+++ src/hooks/useAnimationFrame.js
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react";
+
+export function useAnimationFrame(playing, speed, callback) {
+  const frame = useRef(null);
+  const tick = useRef(0);
+
+  function incrementTick(speed) {
+    let incr;
+    if (speed === 1) incr = 1;
+    if (speed === 2) incr = 2;
+    if (speed === 4) incr = 4;
+    // console.log("increment tick by", incr)
+    tick.current += incr;
+  }
+
+  function animate(frameID) {
+    incrementTick(speed);
+    callback(tick.current);
+    frame.current = requestAnimationFrame(animate);
+  }
+
+  useEffect(() => {
+    if (playing) {
+      frame.current = requestAnimationFrame(animate);
+    } else {
+      cancelAnimationFrame(frame.current);
+    }
+
+    return () => cancelAnimationFrame(frame.current);
+  }, [playing]);
+}
